e83511deb55725b2f9bd676f257f7b2f0270df87
kurumpa <none@none>
2014-03-08 19:05:51 +0200

    server

---
+++ server.js
@@ -0,0 +1,19 @@
+var connect = require('connect');
+var app = connect()
+    .use(function (req, res, next) {
+        "use strict";
+        try {
+            if (req.url.indexOf(".appcache") !== -1) {
+                res.setHeader("Content-Type", "text/cache-manifest");
+            }
+            if (req.url.indexOf("assets/") !== -1) {
+                //Cache-Control "max-age=290304000, no-transform, public"
+                //Expires "Tue, 20 Jan 2037 04:20:42 GMT"
+                // etag off
+            }
+        } finally {
+            next();
+        }
+    })
+    .use(connect.static(__dirname));
+connect.createServer(app).listen(8005);
