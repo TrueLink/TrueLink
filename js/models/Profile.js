8590cb023d89c45c201e2f31e0fd0c2e61f1010b
kurumpa <none@none>
2014-03-14 23:54:39 +0200

    adapting old stuff

--- js/models/Profile.js
+++ js/models/Profile.js
@@ -1,134 +1,21 @@
-define(["zepto"], function ($) {
+define(["zepto", "models/RepoItem", "tools/urandom"], function ($, RepoItem, urandom) {
     "use strict";
-    app.factory("Profile", function (RepoItem) {
-        function Profile(entity, root) {
-            this.entity = entity;
-            this.root = root;
-            this.contacts = createContactsHelper(root, this);
-            this.dialogs = createDialogsHelper(root, this);
-            this.documents = createDocumentsHelper(root, this);
-            this.channels = createChannelsHelper(root, this);
-            this.messages = createMessagesHelper(root, this);
+    function Profile(entity) {
+        this.entity = entity;
 
+    }
+    Profile.prototype = new RepoItem();
+    $.extend(Profile.prototype, {
+        getUserBackground: function () {
+            var bg = this.getData("bg");
+            return parseInt(bg, 10) || 0;
         }
-        Profile.prototype = new RepoItem();
-        angular.extend(Profile.prototype, {
-            getUserBackground: function () {
-                var bg = this.getData("bg");
-                return parseInt(bg, 10) || 0;
-            }
-        });
-
-        // helpers
-        function createContactsHelper(root, profile) {
-            return {
-                getById: function (id) {
-                    return root.contacts.getById(id);
-                },
-                getAll: function () {
-                    return root.contacts.getAll(profile);
-                },
-                getRecent: function (count) {
-                    return root.contacts.getRecent(profile, count);
-                },
-                add: function (contact) {
-                    return root.contacts.add(profile, contact);
-                },
-                getUpdatedEventName: function () {
-                    return root.contacts.getUpdatedEventName();
-                },
-                createContact: function (name) {
-                    return root.contacts.createContact(name);
-                },
-                getByChannel: function (channel) {
-                    return root.contacts.getByChannel(channel);
-                }
-            };
-        }
-        function createDialogsHelper(root, profile) {
-            return {
-                getById: function (id) {
-                    return root.dialogs.getById(id);
-                },
-                getAll: function () {
-                    return root.dialogs.getAll(profile);
-                },
-                getRecent: function (count) {
-                    return root.dialogs.getRecent(profile, count);
-                },
-                add: function (dialog) {
-                    return root.dialogs.add(profile, dialog);
-                },
-                getUpdatedEventName: function () {
-                    return root.dialogs.getUpdatedEventName();
-                },
-                getForContact: function (contact) {
-                    return root.dialogs.getForContact(profile, contact);
-                }
-            };
-        }
-        function createDocumentsHelper(root, profile) {
-            return {
-                getById: function (id) {
-                    return root.documents.getById(id);
-                },
-                getAll: function () {
-                    return root.documents.getAll(profile);
-                },
-                getRecent: function (count) {
-                    return root.documents.getRecent(profile, count);
-                },
-                add: function (document) {
-                    return root.documents.add(profile, document);
-                },
-                getUpdatedEventName: function () {
-                    return root.documents.getUpdatedEventName();
-                },
-                createDocument: function (name) {
-                    return root.documents.createDocument(name);
-                }
-            };
-        }
-        function createChannelsHelper(root, profile) {
-            return {
-                getAll: function () {
-                    return root.channels.getAll(profile).then(function (found) {
-                        return found.map(function (channel) {
-                            channel.profile = profile;
-                            return channel;
-                        });
-                    });
-                },
-                add: function (channel) {
-                    return root.channels.add(profile, channel);
-                },
-                getById: function (id) {
-                    return root.channels.getById(id).then(function (found) {
-                        if (found) {
-                            found.profile = profile;
-                        }
-                        return found;
-                    });
-                },
-                getUpdatedEventName: function () {
-                    return root.channels.getUpdatedEventName();
-                },
-                remove: function (channel) {
-                    return root.channels.remove(profile, channel);
-                }
-            };
-        }
-
-        function createMessagesHelper(root, profile) {
-            return {
-                add: function (dialog, message) {
-                    return root.messages.add(dialog, message);
-                },
-                getAll: function (dialog) {
-                    return root.messages.getAll(dialog);
-                }
-            };
-        }
-        return Profile;
     });
+    Profile.create = function (username, bg) {
+        username = username || urandom.name();
+        bg = bg || 0;
+        return new Profile(new Entity({username: username, bg: bg}));
+    };
+
+    return Profile;
 });
--- js/models/RepoItem.js
+++ js/models/RepoItem.js
@@ -1,40 +1,38 @@
 define(["zepto"], function ($) {
     "use strict";
 
-    app.factory("RepoItem", function () {
 
-        function RepoItem() {
-            this._cachedFields = {};
-        }
+    function RepoItem() {
+        this._cachedFields = {};
+    }
 
-        RepoItem.prototype = {
-            getCacheParams: function () {
-                return {};
-            },
-            getId: function () {
-                return this.entity.id;
-            },
-            getData: function (key) {
-                var cache = this.getCacheParams();
-                if (cache.hasOwnProperty(key)) {
-                    if (!this._cachedFields.hasOwnProperty(key)) {
-                        this._cachedFields[key] = cache[key].load(this.entity.getData(key));
-                    }
-                    return this._cachedFields[key];
+    RepoItem.prototype = {
+        getCacheParams: function () {
+            return {};
+        },
+        getId: function () {
+            return this.entity.id;
+        },
+        getData: function (key) {
+            var cache = this.getCacheParams();
+            if (cache.hasOwnProperty(key)) {
+                if (!this._cachedFields.hasOwnProperty(key)) {
+                    this._cachedFields[key] = cache[key].load(this.entity.getData(key));
                 }
+                return this._cachedFields[key];
+            }
+            return this.entity.getData(key);
+        },
+        setData: function (keyOrValue, value) {
+            var cache = this.getCacheParams();
+            if (value !== undefined && cache.hasOwnProperty(keyOrValue)) {
+                var key = keyOrValue;
+                this._cachedFields[key] = cache[key].set ? cache[key].set(value) : value;
+                this.entity.setData(key, cache[key].save(value));
                 return this.entity.getData(key);
-            },
-            setData: function (keyOrValue, value) {
-                var cache = this.getCacheParams();
-                if (value !== undefined && cache.hasOwnProperty(keyOrValue)) {
-                    var key = keyOrValue;
-                    this._cachedFields[key] = cache[key].set ? cache[key].set(value) : value;
-                    this.entity.setData(key, cache[key].save(value));
-                    return;
-                }
-                return this.entity.setData(keyOrValue, value);
-            },
-        };
-        return RepoItem;
-    });
+            }
+            return this.entity.setData(keyOrValue, value);
+        }
+    };
+    return RepoItem;
 });
