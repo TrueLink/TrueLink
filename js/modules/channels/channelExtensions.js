888d38ff47dc38a52f9be8da63ec5479fd3a1345
kurumpa <none@none>
2014-03-26 17:32:40 +0200

    useful channel extensions

---
+++ js/modules/channels/channelExtensions.js
@@ -0,0 +1,33 @@
+define(["modules/hashTable"], function (HashTable) {
+    "use strict";
+    return {
+        _setPacketSender: function (channel, handler) {
+            channel.setPacketSender({sendPacket: handler.bind(this)});
+        },
+        _setTokenPrompter: function (channel, handler) {
+            channel.setTokenPrompter({prompt: handler.bind(this)});
+        },
+        _setDirtyNotifier: function (channel, handler) {
+            channel.setPacketSender({notify: handler.bind(this)});
+        },
+        _setMsgProcessor: function (channel, handler) {
+            channel.setMsgProcessor({processMessage: handler.bind(this)});
+        },
+        _enterToken: function (token, context) {
+            if (!this._tokenHandlers) {
+                console.warn("No token handler is set for the channel ", this);
+                return;
+            }
+            var handler = this._tokenHandlers.getItem(null, function (item) { return token instanceof item.key; });
+            if (!handler) {
+                console.warn("No token handler is found for the channel ", this);
+                return;
+            }
+            handler(token, context);
+        },
+        _setTokenHandler: function (tokenType, handler) {
+            this._tokenHandlers = this._tokenHandlers || new HashTable();
+            this._tokenHandlers.setItem(tokenType, handler.bind(this));
+        }
+    };
+});
