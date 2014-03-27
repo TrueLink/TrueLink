883ae919e1a2b9747db55ee4d12ebbdd42d479bf
kurumpa <none@none>
2014-03-27 11:14:41 +0200

    Utf8String.fromString / toString()

--- js/modules/data-types/utf8string.js
+++ js/modules/data-types/utf8string.js
@@ -1,7 +1,21 @@
-define(["modules/data-types/multivalue"], function (createType){
-	"use strict";
-	var Utf8String = function (string){
-		this.value = string;
-	};
-	return createType(Utf8String, "utf8string");
+define(["modules/data-types/multivalue"], function (createType) {
+    "use strict";
+    var Utf8String = function (string) {
+        this.value = string;
+    };
+    var mixin = {
+        isEqualTo: function (other) {
+            if (this.constructor.typeName !== other.constructor.typeName) { return false; }
+            return this.value === other.value;
+        },
+        toString: function () {
+            return this.value;
+        }
+    };
+
+    Utf8String.fromString = function (str) {
+        return new Utf8String(str);
+    };
+
+    return createType(Utf8String, "utf8string");
 });
