226bcc8f932c964b75ffa8fdfee0ffc4a16a5817
kurumpa <none@none>
2014-04-01 18:18:01 +0300

    reactjs' invariant

---
+++ js/tools/invariant.js
@@ -0,0 +1,17 @@
+define([], function () {
+    "use strict";
+    return function (condition, format, a, b, c, d, e, f) {
+        if (format === undefined) {
+            throw new Error("invariant requires an error message argument");
+        }
+
+        if (!condition) {
+            var args = [a, b, c, d, e, f];
+            var argIndex = 0;
+            var error = new Error(
+                "Invariant Violation: " + format.replace(/%s/g, function () { return args[argIndex++]; })
+            );
+            throw error;
+        }
+    };
+});
