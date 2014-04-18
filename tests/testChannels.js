ba47e85eb87a92a2b3d436ad95e96863666ff165
evgenus <none@none>
2014-04-18 12:22:08 -0700

    tests for tlke with transport

--- tests/testChannels.js
+++ tests/testChannels.js
@@ -1,12 +1,13 @@
 define([
+    "tools/random", 
+    "modules/data-types/Hex", 
     "modules/channels/EventEmitter",
     "modules/channels/tlke", 
     "modules/channels/tlht", 
-    "tools/random", 
-    "modules/data-types/Hex", 
+    "modules/channels/TestTransport",
     "modules/channels/Route",
-    "zepto"
-], function (EventEmitter, Tlke, Tlht, random, Hex, Route, $) {
+    "zepto",
+], function (random, Hex, EventEmitter, Tlke, Tlht, Transport, Route, $) {
 
     describe("Tlke", function() {
 
@@ -15,21 +16,25 @@ define([
             this._defineEvent("networkPacket");
             this._defineEvent("offer");
             this._defineEvent("auth");
-            var tlke = this.tlke = new Tlke();
-            var route = this.route = new Route();
-            tlke.setRng(random);
-            route.on("packet", tlke.processPacket, tlke);
-            tlke.on("packet", route.processPacket, route);
-            tlke.on("addr", route.setAddr, route);
-            route.on("networkPacket", this.on_sendPacket, this);
-            tlke.on("keyReady", this.keyReady, this);
-
-            tlke.on("offer", this.on_requestOffer, this);
-            tlke.on("auth", this.on_requestAuth, this);
-
+            this._defineEvent("addrIn");
         }
         Builder.prototype = new EventEmitter();
         $.extend(Builder.prototype, {
+            build: function() {
+                var tlke = this.tlke = new Tlke();
+                tlke.setRng(random);
+                var route = this.route = new Route();
+
+                route.on("packet", tlke.processPacket, tlke);
+                route.on("networkPacket", this.on_sendPacket, this);
+                route.on("addrIn", this.on_addrIn, this);
+
+                tlke.on("packet", route.processPacket, route);
+                tlke.on("addr", route.setAddr, route);
+                tlke.on("keyReady", this.keyReady, this);
+                tlke.on("offer", this.on_requestOffer, this);
+                tlke.on("auth", this.on_requestAuth, this);
+            },
             processNetworkPacket: function(packet) {
                 this._log("processNetworkPacket", packet);
                 this.route.processNetworkPacket(packet);
@@ -66,28 +71,97 @@ define([
                     this.fire("auth", auth);
                 }
             },
+            on_addrIn: function(addr) {
+                this._log("on_addrIn", addr);
+                this.fire("addrIn", addr);
+            },
             _log: function() {
-                //console.log(this.name, arguments);
+                console.log(this.name, arguments);
             }
         });
 
-        beforeEach(function() {
-            this.alice = new Builder("Alice");
-            this.bob = new Builder("Bob");
-            this.alice.on("networkPacket", this.bob.processNetworkPacket, this.bob);
-            this.bob.on("networkPacket", this.alice.processNetworkPacket, this.alice);
+        describe("with route short-circuited", function() {
+            beforeEach(function() {
+                var alice = this.alice = new Builder("Alice");
+                var bob = this.bob = new Builder("Bob");
+                alice.on("networkPacket", bob.processNetworkPacket, bob);
+                bob.on("networkPacket", alice.processNetworkPacket, alice);
 
-            this.alice.on("offer", this.bob.enterOffer, this.bob);
-            this.alice.on("auth", this.bob.enterAuth, this.bob);
+                alice.on("offer", bob.enterOffer, bob);
+                alice.on("auth", bob.enterAuth, bob);
+
+                alice.build();
+                bob.build();
+                alice.generate();
+            });
+
+            it("alice key is ready", function() {
+                expect(this.alice.inId).not.toBeUndefined();
+                expect(this.alice.outId).not.toBeUndefined();
+                expect(this.alice.key).not.toBeUndefined();
+            });
+
+            it("bob key is ready", function() {
+                expect(this.bob.inId).not.toBeUndefined();
+                expect(this.bob.outId).not.toBeUndefined();
+                expect(this.bob.key).not.toBeUndefined();
+            });
+
+            it("channel ids matched", function() {
+                expect(this.alice.inId.as(Hex).isEqualTo(this.bob.outId.as(Hex))).toBe(true);
+                expect(this.alice.outId.as(Hex).isEqualTo(this.bob.inId.as(Hex))).toBe(true);
+            });
+
+            it("keys are the same", function() {
+                expect(this.alice.key.as(Hex).isEqualTo(this.bob.key.as(Hex))).toBe(true);
+            });
         });
 
-        it("tlke with route", function() {
-            this.alice.generate();
+        describe("with transport", function() {
+            beforeEach(function() {
+                var transport = this.transport = new Transport();
+                var alice = this.alice = new Builder("Alice");
+                var bob = this.bob = new Builder("Bob");
+                alice.on("networkPacket", transport.sendNetworkPacket, transport);
+                bob.on("networkPacket", transport.sendNetworkPacket, transport);
 
-            this.alice.inId.as(Hex).isEqualTo(this.bob.outId.as(Hex));
-            this.alice.outId.as(Hex).isEqualTo(this.bob.inId.as(Hex));
-            this.alice.key.as(Hex).isEqualTo(this.bob.key.as(Hex));
+                alice.on("addrIn", transport.openAddr, transport);
+                bob.on("addrIn", transport.openAddr, transport);
+
+                transport.on("networkPacket", alice.processNetworkPacket, alice);
+                transport.on("networkPacket", bob.processNetworkPacket, bob);
+
+                alice.on("offer", bob.enterOffer, bob);
+                alice.on("auth", bob.enterAuth, bob);
+
+                alice.build();
+                bob.build();
+                alice.generate();
+            });
+
+            it("alice key is ready", function() {
+                expect(this.alice.inId).not.toBeUndefined();
+                expect(this.alice.outId).not.toBeUndefined();
+                expect(this.alice.key).not.toBeUndefined();
+            });
+
+            it("bob key is ready", function() {
+                expect(this.bob.inId).not.toBeUndefined();
+                expect(this.bob.outId).not.toBeUndefined();
+                expect(this.bob.key).not.toBeUndefined();
+            });
+
+            it("channel ids matched", function() {
+                expect(this.alice.inId.as(Hex).isEqualTo(this.bob.outId.as(Hex))).toBe(true);
+                expect(this.alice.outId.as(Hex).isEqualTo(this.bob.inId.as(Hex))).toBe(true);
+            });
+
+            it("keys are the same", function() {
+                expect(this.alice.key.as(Hex).isEqualTo(this.bob.key.as(Hex))).toBe(true);
+            });
+
         });
+
     });
 
 });
