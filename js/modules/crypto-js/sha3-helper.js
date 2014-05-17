ea7f219f97fb052856e121144e9d5b116b645239
kurumpa <none@none>
2014-05-17 14:16:23 +0300

    tmp
    --HG--
    branch : tmp
    rename : js/components/ContactsPage.js => js/src/ui/ContactsPage.js
    rename : js/components/DocumentsPage.js => js/src/ui/DocumentsPage.js
    rename : js/components/Menu.js => js/src/ui/Menu.js
    rename : js/components/menu/MenuSelectProfile.js => js/src/ui/menu/MenuSelectProfile.js

--- js/modules/crypto-js/sha3-helper.js
+++
@@ -1,4 +0,0 @@
-define(["modules/crypto-js/hasher", "modules/crypto-js/sha3"], function (Hasher, SHA3) {
-    "use strict";
-    return Hasher._createHelper(SHA3);
-});
--- js/modules/data-types/x64word.js
+++
@@ -1,7 +0,0 @@
-define(["modules/data-types/multivalue", "modules/crypto-js/x64word"], function (createType, Word){
-	"use strict";
-	var X64Word = function (high, low){
-		this.value = Word.create(high, low);
-	};
-	return createType(X64Word, "x32wordArray");
-});
