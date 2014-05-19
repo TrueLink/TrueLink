1801186c5f3b914a65d1f80f0737d1949ac5c163
kurumpa <none@none>
2014-05-19 19:01:09 +0300

    model's getter/setter

--- js/mixins/onChanged.js
+++ js/mixins/onChanged.js
@@ -1,6 +1,13 @@
 define(function (require, exports, module) {
     "use strict";
+
     module.exports = {
-        onChanged: function () { this.fire("changed", this); }
+        onChanged: function () { this.fire("changed", this); },
+        set: function (name, value) {
+            if (this[name] === value) { return; }
+            this[name] = value;
+            this.onChanged();
+        },
+        get: function (name) { return this[name]; }
     };
 });
