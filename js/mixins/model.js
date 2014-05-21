6a78b12c4f4ec6eb9b3433fe5c9141dc91a27e1d
kurumpa <none@none>
2014-05-21 19:25:48 +0300

    model.set() now accepts objects

--- js/mixins/model.js
+++ js/mixins/model.js
@@ -4,8 +4,16 @@ define(function (require, exports, module) {
     module.exports = {
         onChanged: function () { this.fire("changed", this); },
         set: function (name, value) {
-            if (this[name] === value) { return; }
-            this[name] = value;
+            if (typeof name === "object") {
+                for (var key in name) {
+                    if(name.hasOwnProperty(key)) {
+                        this[key] = name[key];
+                    }
+                }
+            } else {
+                if (this[name] === value) { return; }
+                this[name] = value;
+            }
             this.onChanged();
         },
         get: function (name) { return this[name]; },
