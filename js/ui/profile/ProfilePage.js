57700e977d511073e9aa514cd85df5f61a47bf18
kurumpa <none@none>
2014-05-21 15:16:28 +0300

    Profile settings page

---
+++ js/ui/profile/ProfilePage.js
@@ -0,0 +1,22 @@
+define(function (require, exports, module) {
+    "use strict";
+    var React = require("react");
+    module.exports = React.createClass({
+        displayName: "ProfilePage",
+        getInitialState: function () {
+            return this._getState();
+        },
+        _getState: function () {
+            var model = this.props.model;
+            return {
+                profile: model
+            };
+        },
+        _onModelChanged: function () { this.setState(this._getState()); },
+        componentDidMount: function () { this.props.model.on("changed", this._onModelChanged, this); },
+        componentWillUnmount: function () { this.props.model.off("changed", this._onModelChanged); },
+        render: function () {
+            return React.DOM.div(null, "Profile Settings: " + this.state.profile.name);
+        }
+    });
+});
