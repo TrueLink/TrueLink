1c6227de995909bb590316dab2161737e055e8c1
kurumpa <none@none>
2014-05-21 15:17:31 +0300

    default page not needed in Router

--- js/models/Router.js
+++ js/models/Router.js
@@ -10,7 +10,8 @@ define(function (require, exports, module) {
         "home": require("ui/home/HomePage"),
         "contacts" : require("ui/contacts/ContactsPage"),
         "dialogs" : require("ui/dialogs/DialogsPage"),
-        "documents" : require("ui/documents/DocumentsPage")
+        "documents" : require("ui/documents/DocumentsPage"),
+        "profile" : require("ui/profile/ProfilePage")
     };
 
     function Router(factory) {
@@ -18,9 +19,7 @@ define(function (require, exports, module) {
         this.factory = factory;
         this._defineEvent("changed");
         this.currentPage = null;
-        this.defaultPageName = null;
         this.currentPageName = null;
-        this.defaultPageModel = null;
         this.currentPageModel = null;
     }
 
@@ -29,49 +28,25 @@ define(function (require, exports, module) {
         serialize: function (packet, context) {
             console.log("serialize Router");
             packet.setData({
-                pageName: this.currentPageName,
-                defaultPageName: this.defaultPageName
+                pageName: this.currentPageName
             });
             if (this.currentPageModel) {
                 packet.setData({
-                    modelType: this.factory.getTypeData(this.currentPageModel),
-                    defaultModelType: this.factory.getTypeData(this.defaultPageModel)
+                    modelType: this.factory.getTypeData(this.currentPageModel)
                 });
             }
             packet.setLink("model", context.getPacket(this.currentPageModel));
-            packet.setLink("defaultModel", context.getPacket(this.defaultPageModel));
         },
         deserialize: function (packet, context) {
             console.log("deserialize Router");
             var data = packet.getData();
             this.currentPageName = data.pageName;
-            this.defaultPageName = data.defaultPageName;
             var factory = this.factory;
 
-            if (data.modelType !== undefined) {
-                try {
-                    var modelConstructor = factory.getConstructor(data.modelType);
-                    this.currentPageModel = context.deserialize(packet.getLink("model"), modelConstructor.bind(factory));
-                    this.currentPage = this._createPage(this.currentPageName, this.currentPageModel);
-                } catch (ex) {
-                    var defaultModelConstructor = factory.getConstructor(data.defaultModelType);
-                    this.defaultPageModel = context.deserialize(packet.getLink("model"), defaultModelConstructor.bind(factory));
-                    this.currentPage = this._createPage(this.defaultPageName, this.defaultPageModel);
-                }
-            }
+            var modelConstructor = factory.getConstructor(data.modelType);
+            this.currentPageModel = context.deserialize(packet.getLink("model"), modelConstructor.bind(factory));
+            this.currentPage = this._createPage(this.currentPageName, this.currentPageModel);
         },
-
-        setDefaultPage: function (pageName, pageModel) {
-            this.defaultPageName = pageName;
-            this.defaultPageModel = pageModel;
-
-            if (!this.currentPage) {
-                this.navigate(pageName, pageModel);
-            } else {
-                this.onChanged();
-            }
-        },
-
 
         navigate: function (pageName, pageModel) {
             try {
