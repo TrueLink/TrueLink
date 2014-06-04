ef7eb5f5dcead0e8969fec66e90316e520acf815
kurumpa <none@none>
2014-06-04 08:20:52 +0300

    crypto-js and decBlocks converters

--- js/converters/crypto-js.js
+++ js/converters/crypto-js.js
@@ -1,32 +1,31 @@
-define([
-    "modules/converters/multiconverter",
-    "modules/crypto-js/enc-utf8",
-    "modules/crypto-js/enc-base64",
-    "modules/crypto-js/enc-hex",
-    "modules/data-types/x32wordArray",
-    "modules/data-types/utf8string",
-    "modules/data-types/base64",
-    "modules/data-types/hex"
-],
-    function (converter, encUTF8, encBase64, encHex, X32WordArray, Utf8String, Base64, Hex) {
-        "use strict";
-        converter.register("utf8string", "x32wordArray", function (value) {
-            return new X32WordArray(encUTF8.parse(value));
-        });
-        converter.register("x32wordArray", "utf8string", function (value) {
-            return new Utf8String(encUTF8.stringify(value));
-        }, true);
-        converter.register("base64", "x32wordArray", function (value) {
-            return new X32WordArray(encBase64.parse(value));
-        });
-        converter.register("x32wordArray", "base64", function (value) {
-            return new Base64(encBase64.stringify(value));
-        });
-        converter.register("hex", "x32wordArray", function (value) {
-            return new X32WordArray(encHex.parse(value));
-        });
-        converter.register("x32wordArray", "hex", function (value) {
-            return new Hex(encHex.stringify(value));
-        });
+define(function (require, exports, module) {
+    "use strict";
+    var converter = require("modules/multivalue/converter").getInstance();
+    var encUTF8 = require("modules/crypto-js/enc-utf8");
+    var encBase64 = require("modules/crypto-js/enc-base64");
+    var encHex = require("modules/crypto-js/enc-hex");
+    var X32WordArray = require("modules/multivalue/x32wordArray");
+    var Utf8String = require("modules/multivalue/utf8string");
+    var Base64 = require("modules/multivalue/base64");
+    var Hex = require("modules/multivalue/hex");
+
+    converter.register("utf8string", "x32wordArray", function (value) {
+        return new X32WordArray(encUTF8.parse(value));
+    });
+    converter.register("x32wordArray", "utf8string", function (value) {
+        return new Utf8String(encUTF8.stringify(value));
+    }, true);
+    converter.register("base64", "x32wordArray", function (value) {
+        return new X32WordArray(encBase64.parse(value));
+    });
+    converter.register("x32wordArray", "base64", function (value) {
+        return new Base64(encBase64.stringify(value));
+    });
+    converter.register("hex", "x32wordArray", function (value) {
+        return new X32WordArray(encHex.parse(value));
+    });
+    converter.register("x32wordArray", "hex", function (value) {
+        return new Hex(encHex.stringify(value));
+    });
 
 });
--- js/converters/customTypes.js
+++ js/converters/customTypes.js
@@ -1,21 +1,20 @@
-define([
-    "modules/converters/multiconverter",
-    "modules/data-types/decBlocks",
-    "modules/data-types/hex",
-    "modules/leemon/BigInt"
-],
-    function (converter, DecBlocks, Hex, leemon) {
-        "use strict";
-        converter.register("hex", "decBlocks", function (value) {
-            var bi = leemon.str2bigInt(value, 16);
-            return new DecBlocks(leemon.bigInt2str(bi, 10));
-        });
-        converter.register("decBlocks", "hex", function (value) {
-            var bi = leemon.str2bigInt(value, 10);
-            var hex = new Hex(leemon.bigInt2str(bi, 16));
-            if (value.minBitLength) {
-                hex.expand(value.minBitLength);
-            }
-            return hex;
-        });
+define(function (require, exports, module) {
+    "use strict";
+    var converter = require("modules/multivalue/converter").getInstance();
+    var DecBlocks = require("modules/multivalue/decBlocks");
+    var Hex = require("modules/multivalue/hex");
+    var leemon = require("modules/leemon/BigInt");
+
+    converter.register("hex", "decBlocks", function (value) {
+        var bi = leemon.str2bigInt(value, 16);
+        return new DecBlocks(leemon.bigInt2str(bi, 10));
     });
+    converter.register("decBlocks", "hex", function (value) {
+        var bi = leemon.str2bigInt(value, 10);
+        var hex = new Hex(leemon.bigInt2str(bi, 16));
+        if (value.minBitLength) {
+            hex.expand(value.minBitLength);
+        }
+        return hex;
+    });
+});
