6509cff4a14152c641a4a6301515479b5290e84c
kurumpa <none@none>
2014-06-18 21:34:14 +0300

    factory.getInstance() for singletons

--- js/serialization/factories/prototype.js
+++ js/serialization/factories/prototype.js
@@ -3,8 +3,10 @@ define(function (require, exports, module) {
     var invariant = require("modules/invariant");
     var extend = require("extend");
     var model = require("mixins/model");
+    var tools = require("modules/tools");
 
     module.exports = {
+        singletons: {},
         _observed: function (obj) {
             invariant(this.serializer, "serializer is not defined");
             // todo this is kinda strange
@@ -13,6 +15,15 @@ define(function (require, exports, module) {
             }
             this.serializer.listen(obj);
             return obj;
+        },
+        getInstance: function (name, constructor, thisArg) {
+            if (!this.singletons[name]) {
+                if (!tools.isFunction(constructor)) {
+                    throw new Error("No " + name + " instance created yet");
+                }
+                this.singletons[name] = constructor.call(thisArg);
+            }
+            return this.singletons[name];
         }
     };
 });
