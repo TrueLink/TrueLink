f8474d1b5e72a628767cc419f3ecac19b8dcb7df
Eugene Chernyshov <evgenus@main.dev>
2014-07-14 03:02:35 +0300

    better tests for multivalue

--- tests/testMultivalue.js
+++ tests/testMultivalue.js
@@ -5,6 +5,7 @@ define(function(require, exports, module) {
     var Base64Url = require("modules/multivalue/base64url");
     var BigIntSjcl = require("modules/multivalue/bigIntSjcl");
     var BigIntForge = require("modules/multivalue/bigIntForge");
+    var ByteBuffer = require("modules/multivalue/byteBuffer");
     var Bytes = require("modules/multivalue/bytes");
     var DecBlocks = require("modules/multivalue/decBlocks");
     var Hex = require("modules/multivalue/hex");
@@ -12,16 +13,17 @@ define(function(require, exports, module) {
     var X32WordArray = require("modules/multivalue/x32wordArray");
 
     var values = {
-        BitArray: new BitArray([1400140393, 1852252269, 1702064993, 1734680881]),
-        Base64: new Base64("U3RyaW5nIG1lc3NhZ2UhMQ=="),
-        Base64Url: new Base64Url("U3RyaW5nIG1lc3NhZ2UhMQ"),
-        BigIntSjcl: new BigIntSjcl("0x537472696E67206D6573736167652131"),
-        BigIntForge: new BigIntForge("537472696E67206D6573736167652131"),
-        Bytes: new Bytes([83, 116, 114, 105, 110, 103, 32, 109, 101, 115, 115, 97, 103, 101, 33, 49]),
-        DecBlocks: new DecBlocks("110930550633557961317610434596264288561"),
-        Hex: new Hex("537472696E67206D6573736167652131"),
-        Utf8String: new Utf8String("String message!1"), 
-        X32WordArray: new X32WordArray([1400140393, 1852252269, 1702064993, 1734680881], 16),
+        BitArray: new BitArray([1400140393, 1852252269, 1702064993, 1734680881, -779103857, 17591406886912]),
+        Base64: new Base64("U3RyaW5nIG1lc3NhZ2UhMdGP0Y/Rjw=="),
+        Base64Url: new Base64Url("U3RyaW5nIG1lc3NhZ2UhMdGP0Y_Rjw"),
+        BigIntSjcl: new BigIntSjcl("0x537472696E67206D6573736167652131D18FD18FD18F"),
+        BigIntForge: new BigIntForge("537472696E67206D6573736167652131D18FD18FD18F"),
+        Bytes: new Bytes([83, 116, 114, 105, 110, 103, 32, 109, 101, 115, 115, 97, 103, 101, 33, 49, 209, 143, 209, 143, 209, 143]),
+        DecBlocks: new DecBlocks("31224174156080973347525092013460821321037759413997967"),
+        Hex: new Hex("537472696E67206D6573736167652131D18FD18FD18F"),
+        ByteBuffer: new ByteBuffer("String message!1\xd1\x8f\xd1\x8f\xd1\x8f"),
+        Utf8String: new Utf8String("String message!1яяя"), 
+        X32WordArray: new X32WordArray([1400140393, 1852252269, 1702064993, 1734680881, -779103857, 17591406886912], 22),
     }
     var types = {
         BitArray: BitArray,
@@ -29,6 +31,7 @@ define(function(require, exports, module) {
         Base64Url: Base64Url,
         BigIntSjcl: BigIntSjcl,
         BigIntForge: BigIntForge,
+        ByteBuffer: ByteBuffer,
         Bytes: Bytes,
         DecBlocks: DecBlocks,
         Hex: Hex,
