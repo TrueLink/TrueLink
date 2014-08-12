a5d416cdb3985455d043edfb0657414fe5252a1d
murashko <none@none>
2014-08-12 07:02:46 -0700

    declare modules from vendor
    --HG--
    branch : typescript_migration

--- ts/typings/flux-react.d.ts
+++ ts/typings/flux-react.d.ts
@@ -1,6 +1,7 @@
 interface Window {
     app: any;
     fakeDb: any;
+    profile: any;
 }
 
 /*   interface EventEmitter {
@@ -15,13 +16,142 @@ interface Window {
 
 declare var $ : any;
 
-declare module "react" {
+declare module "modules/channels/OverTlecBuilder" {
     export = $;
 }
-declare module "zepto" {
+declare module "modules/channels/Route" {
+    export = $;
+}
+declare module "modules/channels/Tlec" {
+    export = $;
+}
+declare module "modules/channels/TlecBuilder" {
+    export = $;
+}
+declare module "modules/channels/Tlgr" {
+    export = $;
+}
+declare module "modules/channels/Tlht" {
+    export = $;
+}
+declare module "modules/channels/TlhtBuilder" {
+    export = $;
+}
+declare module "modules/channels/Tlke" {
+    export = $;
+}
+declare module "modules/channels/TlkeBuilder" {
+    export = $;
+}
+declare module "modules/crypto-js/enc-base64" {
+    export = $;
+}
+declare module "modules/crypto-js/enc-hex" {
+    export = $;
+}
+declare module "modules/crypto-js/enc-utf8" {
+    export = $;
+}
+declare module "modules/cryptography/random" {
+    export = $;
+}
+declare module "modules/dictionary/dictionary" {
     export = $;
 }
 declare module "modules/events/eventEmitter" {
+    export = $;
+}
+declare module "modules/filter/Filter" {
+    export = $;
+}
+declare module "modules/forge" {
+    export = $;
+}
+declare module "modules/invariant" {
+    export = $;
+}
+declare module "modules/leemon/BigInt" {
+    export = $;
+}
+declare module "modules/multivalue/base64" {
+    export = $;
+}
+declare module "modules/multivalue/base64url" {
+    export = $;
+}
+declare module "modules/multivalue/bigIntForge" {
+    export = $;
+}
+declare module "modules/multivalue/bigIntSjcl" {
+    export = $;
+}
+declare module "modules/multivalue/bitArray" {
+    export = $;
+}
+declare module "modules/multivalue/byteBuffer" {
+    export = $;
+}
+declare module "modules/multivalue/bytes" {
+    export = $;
+}
+declare module "modules/multivalue/converter" {
+    export = $;
+}
+declare module "modules/multivalue/decBlocks" {
+    export = $;
+}
+declare module "modules/multivalue/hex" {
+    export = $;
+}
+declare module "modules/multivalue/multivalue" {
+    export = $;
+}
+declare module "modules/multivalue/utf8string" {
+    export = $;
+}
+declare module "modules/multivalue/x32wordArray" {
+    export = $;
+}
+declare module "modules/serialization/SerializationContext" {
+    export = $;
+}
+declare module "modules/serialization/SerializationPacket" {
+    export = $;
+}
+declare module "modules/serialization/serializable" {
+    export = $;
+}
+declare module "modules/sjcl/bitArray" {
+    export = $;
+}
+declare module "modules/sjcl/bn" {
+    export = $;
+}
+declare module "modules/sjcl/codecBase64" {
+    export = $;
+}
+declare module "modules/sjcl/codecBytes" {
+    export = $;
+}
+declare module "modules/sjcl/codecHex" {
+    export = $;
+}
+declare module "modules/sjcl/codecString" {
+    export = $;
+}
+declare module "modules/tools" {
+    export = $;
+}
+declare module "modules/urandom/urandom" {
+    export = $;
+}
+declare module "react" {
+    export = $;
+}
+declare module "uuid" {
+    export = $;
+}
+declare module "zepto" {
     export = $;
 }
 
