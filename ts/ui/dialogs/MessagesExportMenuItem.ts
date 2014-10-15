b78de30ce425ff35d8310cd4a74c4196a3a22620
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2014-10-15 06:09:17 +0300

    ITlgrInvitationMessage export
    --HG--
    branch : export-history

--- ts/ui/dialogs/MessagesExportMenuItem.ts
+++ ts/ui/dialogs/MessagesExportMenuItem.ts
@@ -10,25 +10,29 @@
             return false;
         },
 
+        _renderMessageToExport: function(message: IUserMessage) {
+            if (message.type === "tlgr-invite") {
+                var tlgrInvitationMessage = <ITlgrInvitationMessage>message;
+                if ((<any>tlgrInvitationMessage).accepted === true) {
+                    return "You have accepted invitation to group chat from " + tlgrInvitationMessage.sender;
+                }
+                if ((<any>tlgrInvitationMessage).accepted === false) {
+                    return "You have rejected invitation to group chat from " + tlgrInvitationMessage.sender;
+                }
+                return message.sender + " invited you to group chat. You did not react yet.";
+            }
+            else {
+                var textMessage = <ITextMessage>message;
+                if(textMessage.isMine) {
+                    return textMessage.sender + " (me): " + textMessage.text;
+                } else {
+                    return (textMessage.sender || "unknown") + ": " + textMessage.text;
+                }
+            }
+        },
+
         render: function () {
-
-            var historyToExport = this.props.messages
-                .map(message => {
-                    if (message.type === "tlgr-invite") {
-                        //todo complete
-                        var tlgrInvitationMessage = <ITlgrInvitationMessage>message;
-                        return "tlgr-invite";
-                    }
-                    else {
-                        var textMessage = <ITextMessage>message;
-                        if(textMessage.isMine) {
-                            return textMessage.sender + " (me): " + textMessage.text;
-                        } else {
-                            return (textMessage.sender || "unknown") + ": " + textMessage.text;
-                        }
-                    }
-                }).join("\n");
-
+            var historyToExport = this.props.messages.map(this._renderMessageToExport).join("\n");
             return ReactBootstrap.MenuItem({
                 href: "data:text/plain;charset=utf-8," + encodeURIComponent(historyToExport),
                 onClick: this._onClick
