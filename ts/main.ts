bd68b5f0163394ce7286ee90a1bf48c1217cbda3
Dmitry Murahko <dvmurashko@main.dev>
2014-10-23 05:09:17 -0700

    .
    --HG--
    branch : spa-ify

--- ts/main.ts
+++ ts/main.ts
@@ -2,6 +2,7 @@
 /// <reference path="../vendor/typings/require/require.d.ts"/>
 /// <reference path="../vendor/typings/es6-promises/es6-promises.d.ts"/>
 /// <reference path="../vendor/typings/localforage/localforage.d.ts"/>
+/// <reference path="../node_modules/react-typescript-definitions/react.d.ts"/>
 import Application = require("./models/App");
 import AppComponent = require("./ui/AppComponent");
 import Serializer = require("./serialization/serializer");
