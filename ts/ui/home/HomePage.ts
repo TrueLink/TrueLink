37c756f6d5c9b8e460b95485dcf66448f4d965bf
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2014-10-24 01:35:16 +0300

    home page context menu stub
    --HG--
    branch : context-menus

--- ts/ui/home/HomePage.ts
+++ ts/ui/home/HomePage.ts
@@ -1,12 +1,12 @@
     "use strict";
     import React = require("react");
+    import ReactBootstrap = require("react-bootstrap");
     import reactObserver = require("../../mixins/reactObserver");
     var exp = React.createClass({
         displayName: "HomePage",
         mixins: [reactObserver],
         render: function () {
             var app = this.state.model;
-//            var pageModel = this.state.pageModel;
             var router = this.props.router;
             var profileComponents = {};
             var model = this.props.pageModel.model;
@@ -18,10 +18,45 @@
             });
             return React.DOM.div(null,
                 React.DOM.div({className: "app-page-header"},
+                    React.DOM.span({className: "header-dropdown-menu-button"},
+                        ReactBootstrap.DropdownButton({
+                                title: "∴",
+                                pullRight: true,
+                                onSelect: function () {} // menu does not close on item click without this
+                            },
+                            ReactBootstrap.MenuItem({
+                                onClick: function () {/* stub */}
+                            }, "(stub) Start dialog"),
+                            ReactBootstrap.MenuItem({
+                                onClick: function () {/* stub */}
+                            }, "(stub) Add contact"),
+                            ReactBootstrap.MenuItem({
+                                divider: true
+                            }),
+                            ReactBootstrap.MenuItem({
+                                onClick: function () {/* stub */}
+                            }, "(stub) Accept invitation"),
+                            ReactBootstrap.MenuItem({
+                                onClick: function () {/* stub */}
+                            }, "(stub) Enter confirmation"),
+                            ReactBootstrap.MenuItem({
+                                onClick: function () {/* stub */}
+                            }, "(stub) Connet to a public key"),
+                            ReactBootstrap.MenuItem({
+                                divider: true
+                            }),
+                            ReactBootstrap.MenuItem({
+                                onClick: function () {/* stub */}
+                            }, "(stub) Advanced"),
+                            ReactBootstrap.MenuItem({
+                                onClick: function () {/* stub */}
+                            }, "(stub) Settings"),
+                            ReactBootstrap.MenuItem({
+                                onClick: function () {/* stub */}
+                            }, "(stub) Help"))),
                     React.DOM.a({
                         className: "title",
-                        href: ""//,
-                      //  onClick: router.createNavigateHandler("home", app)
+                        href: "#"
                     }, "Home page: " + app.currentProfile.name))
                 ,
                 React.DOM.ul(null, profileComponents));
