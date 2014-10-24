3f87ac95ffe9e7cfde511fb0287146e86c325ad5
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2014-10-24 06:59:29 +0300

    profile creation main page markup
    --HG--
    branch : onboarding
    extra : amend_source : 058d174242ddfc7db9e210db1a5bce04dfbb28a6

--- ts/ui/profile/ProfileCreationMainPage.ts
+++ ts/ui/profile/ProfileCreationMainPage.ts
@@ -3,7 +3,34 @@ import React = require("react");
 var exp = React.createClass({
     displayName: "ProfileCreationMainPage",
     render: function () {
-        return React.DOM.div();
+        return React.DOM.div({
+                className: "profile-creation"
+            },
+            React.DOM.h1(null, "TrueLink"),
+            React.DOM.h2(null, "Create new profile:"),
+            React.DOM.a({
+                    className: "button profile-creation-anonymous",
+                    href: "#"
+                }, "Anonymous"),
+            React.DOM.p(null, "Nothing is known about you. No one can initiate contact with you."),
+            React.DOM.a({
+                    className: "button profile-creation-pseudonymous",
+                    href: "#"
+                }, "Pseudonymous"),
+            React.DOM.p(null, "Your contacts know nothing but your nickname. A link that allows initiating conacts with you is generated."),
+            React.DOM.a({
+                    className: "button profile-creation-public",
+                    href: "#"
+                }, "Public Account"),
+            React.DOM.p(null, "Your contacts know your name, email and phone number. A link that allows initiating conacts with you is generated."),
+            React.DOM.div({
+                    className: "profile-creation-footer"
+                },
+                React.DOM.p(null, "Do you have a profile on another device already?"),
+                React.DOM.a({
+                    className: "button",
+                    href: "#"
+                }, "Sign into existing profile")));
     }
 });
 export = exp;
