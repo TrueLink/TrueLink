82406ac446684164f607ab63662e4c917825520a
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2014-10-24 22:38:57 +0300

    profile creation main page hmtl/css done
    --HG--
    branch : onboarding

--- ts/ui/profile/ProfileCreationMainPage.ts
+++ ts/ui/profile/ProfileCreationMainPage.ts
@@ -3,30 +3,36 @@ import React = require("react");
 var exp = React.createClass({
     displayName: "ProfileCreationMainPage",
     render: function () {
-        return React.DOM.div({
-                className: "profile-creation"
-            },
-            React.DOM.h1(null, "TrueLink"),
-            React.DOM.h2(null, "Create new profile:"),
-            React.DOM.a({
-                    className: "button profile-creation-anonymous",
-                    href: "#"
-                }, "Anonymous"),
-            React.DOM.p(null, "Nothing is known about you. No one can initiate contact with you."),
-            React.DOM.a({
-                    className: "button profile-creation-pseudonymous",
-                    href: "#"
-                }, "Pseudonymous"),
-            React.DOM.p(null, "Your contacts know nothing but your nickname. A link that allows initiating conacts with you is generated."),
-            React.DOM.a({
-                    className: "button profile-creation-public",
-                    href: "#"
-                }, "Public Account"),
-            React.DOM.p(null, "Your contacts know your name, email and phone number. A link that allows initiating conacts with you is generated."),
-            React.DOM.div({
-                    className: "profile-creation-footer"
-                },
-                React.DOM.p(null, "Do you have a profile on another device already?"),
+        var router = this.props.router;
+        //var profile = this.state.model;
+
+        return  React.DOM.div({className: "profile-creation-page app-page"},
+            React.DOM.div({className: "app-page-content has-footer"},                                 
+                React.DOM.h1(null, "TrueLink"),
+                React.DOM.h2(null, "Create new profile:"),
+                React.DOM.a({
+                        className: "button profile-creation-anonymous",
+                        href: "#"
+                    }, "Anonymous"),
+                React.DOM.p({
+                        className: "description"
+                    }, "Nothing is known about you. No one can initiate contact with you."),
+                React.DOM.a({
+                        className: "button profile-creation-pseudonymous",
+                        href: "#"
+                    }, "Pseudonymous"),
+                React.DOM.p({
+                        className: "description"
+                    }, "Your contacts know nothing but your nickname. A link that allows initiating conacts with you is generated."),
+                React.DOM.a({
+                        className: "button profile-creation-public",
+                        href: "#"
+                    }, "Public Account"),
+                React.DOM.p({
+                        className: "description"
+                    }, "Your contacts know your name, email and phone number. A link that allows initiating conacts with you is generated.")),
+            React.DOM.div({ className: "app-page-footer" },
+                React.DOM.p(null, "Or do you have a profile on another device already?"),
                 React.DOM.a({
                     className: "button",
                     href: "#"
