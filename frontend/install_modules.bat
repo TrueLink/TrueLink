878c54c9542c7389e7e532b95a63e29b178f6ba4
Dmitry Murahko <dvmurashko@main.dev>
2014-10-29 07:10:26 -0700

    install modules bat
    --HG--
    branch : typescript_modules

---
+++ frontend/install_modules.bat
@@ -0,0 +1,11 @@
+npm install ..\Filter & ^
+ npm install ..\OverTlecBuilder &^
+ npm install ..\Route &^
+ npm install ..\SerializationContext &^
+ npm install ..\TestTransport &^
+ npm install ..\TlecBuilder &^
+ npm install ..\Tlgr &^
+ npm install ..\Tlht &^
+ npm install ..\TlhtBuilder &^
+ npm install ..\TlkeBuilder &^
+ npm install ..\modules
