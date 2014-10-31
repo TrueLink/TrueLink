3bc78ea0d042d01e8cfebd2985346859cdbd82c1
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2014-10-31 19:10:40 +0200

    cutting Algo out of Tlke
    --HG--
    branch : cutting Algo out of Tlke

--- modules/channels/Tlke.js
+++ modules/channels/Tlke.js
@@ -1,9 +1,12 @@
 define(function (require, exports, module) {
     "use strict";
 
-    Tlke.authBitLength = 16;
-    Tlke.offerBitLength = 128;
-    Tlke.dhPrivBitLength = 160;
+    Algo.authBitLength = 16;
+    Algo.offerBitLength = 128;
+    Algo.dhPrivBitLength = 160;
+    Tlke.authBitLength = Algo.authBitLength;
+    Tlke.offerBitLength = Algo.offerBitLength;
+    Tlke.dhPrivBitLength = Algo.dhPrivBitLength;
 
     var tools = require("../tools");
     var SHA1 = require("../cryptography/sha1-crypto-js");
@@ -26,6 +29,195 @@ define(function (require, exports, module) {
         return SHA1(value).as(BitArray).bitSlice(0, 128);
     }
 
+// __________________________________________________________________________ //
+
+    function Algo(random) {
+        this.random = random;
+
+        this.state = null;
+        this._dhAesKey = null;
+        this.dhk = null;
+        this.dh = null;
+        this.auth = null;
+        this.check = null;
+        this.authData = null;
+    }
+
+    Algo.prototype.init = function () {
+        this.state = Algo.STATE_NOT_STARTED;
+    }
+
+
+    Algo.prototype._getRandomBytes = function (bitLength) {
+        invariant(isFunction(this.random.bitArray), "random must implement IRandom");
+        return this.random.bitArray(bitLength);
+    }
+
+    Algo.prototype._encrypt = function (bytes, customKey) {
+        var iv = this._getRandomBytes(128);
+        var aes = new Aes(customKey || this._dhAesKey);
+        var encryptedData = aes.encryptCbc(bytes, iv);
+        return iv.as(Bytes).concat(encryptedData);
+    }
+
+    Algo.prototype._decrypt = function (bytes, customKey) {
+        var dataBitArray = bytes.as(BitArray);
+        var iv = dataBitArray.bitSlice(0, 128);
+        var encryptedData = dataBitArray.bitSlice(128, dataBitArray.bitLength());
+        var aes = new Aes(customKey || this._dhAesKey);
+        return aes.decryptCbc(encryptedData, iv);
+    }
+
+    // Alice 1.1 (instantiation)
+    Algo.prototype._generateOffer = function () {
+        this.dh = DiffieHellman.generate(Algo.dhPrivBitLength, this.random);
+        var dhAes = this._getRandomBytes(Algo.offerBitLength);
+        this._dhAesKey = dhAes;
+        var outId = dhAes.bitSlice(0, 16);
+        var inId = dhAes.bitSlice(16, 32);
+    }
+
+    // Bob 2.1 (instantiation) offer is from getOffer (via IM)
+    Algo.prototype._acceptOffer = function (offer) {
+        this.dh = DiffieHellman.generate(Algo.dhPrivBitLength, this.random);
+        var dhAes = offer.as(Hex).as(BitArray);
+        this._dhAesKey = dhAes;
+        var inId = dhAes.bitSlice(0, 16);
+        var outId = dhAes.bitSlice(16, 32);
+        this.state = Algo.STATE_AWAITING_OFFERDATA;
+        return {inId: inId, outId: outId};
+    }
+
+
+    Algo.prototype._getOfferData = function () {
+        var dhData = new Hex(this.dh.createKeyExchange());
+        return this._encrypt(dhData);
+    }
+
+    // Bob 2.2.
+    Algo.prototype._acceptOfferData = function (bytes) {
+        var dhData;
+        try {
+            dhData = this._decrypt(bytes);
+        } catch (ex) {
+            console.warn("Received bad bytes.  " + ex.message);
+            return;
+        }
+        var dhDataHex = dhData.as(Hex).value;
+        var dhkHex = this.dh.decryptKeyExchange(dhDataHex);
+        this.dhk = new Hex(dhkHex);
+        this.state = Tlke.STATE_AWAITING_AUTH;
+    }
+
+    Algo.prototype._getOfferResponse = function () {
+        var dhData = new Hex(this.dh.createKeyExchange());
+        return this._encrypt(dhData);
+    }
+
+    // Alice 3.1
+    Algo.prototype._acceptOfferResponse = function (data) {
+        var dhDataHex;
+        try {
+            dhDataHex = this._decrypt(data).as(Hex).value;
+        } catch (ex) {
+            console.warn("Received bad bytes.  " + ex.message);
+            return;
+        }
+        var dhkHex = this.dh.decryptKeyExchange(dhDataHex);
+        this.dhk = new Hex(dhkHex);
+
+        this.auth = this._getRandomBytes(Tlke.authBitLength);
+        this.check = this._getRandomBytes(128);
+        this.state = Tlke.STATE_AWAITING_AUTH_RESPONSE;
+    }
+
+    Algo.prototype._getAuthData = function () {
+        return this._encrypt(this.check, this._getVerifiedDhk());
+    }
+
+    Algo.prototype._getVerifiedDhk = function () {
+        var dhk = this.dhk.as(Bytes);
+        var auth = this.auth.as(Bytes);
+        return hash(dhk.concat(auth));
+    }
+
+    // Bob 4.2
+    Algo.prototype._acceptAuthData = function (bytes) {
+        this.authData = bytes;
+        if (this.auth) {
+            this._acceptAuthAndData();
+        } else {
+            this.state = Tlke.STATE_AWAITING_AUTH;
+        }
+    }
+
+    // Bob 4.1
+    Algo.prototype._acceptAuth = function (auth) {
+        this.auth = auth;
+        if (this.authData) {
+            this._acceptAuthAndData();
+        } else {
+            this.state = Tlke.STATE_AWAITING_AUTHDATA;
+        }
+    }
+
+    // Bob 4.3 (4.1 + 4.2)
+    Algo.prototype._acceptAuthAndData = function () {
+        var bytes = this.authData;
+        // todo check's checksum and ACHTUNG if not match
+        var verified = this._getVerifiedDhk();
+        try {
+            this.check = this._decrypt(bytes, verified);
+        } catch (ex) {
+            console.warn("Received bad bytes.  " + ex.message);
+            return;
+        }
+        this.state = Tlke.STATE_CONNECTION_ESTABLISHED;
+        return {
+            inId: hCheck.bitSlice(0, 16),
+            outId: hCheck.bitSlice(16, 32),
+            key: hash(this.check.as(Bytes).concat(verified))
+        };
+    }
+
+    Algo.prototype._getAuthResponse = function () {
+        var hCheck = hash(this.check);
+        return this._encrypt(hCheck, this._getVerifiedDhk());
+    }
+
+    // Alice 5
+    Algo.prototype._acceptAuthResponse = function (bytes) {
+        var verified = this._getVerifiedDhk(), hCheck;
+        try {
+            hCheck = this._decrypt(bytes, verified);
+        } catch (ex) {
+            console.warn("Received bad bytes.  " + ex.message);
+            return;
+        }
+        if (hash(this.check).as(Hex).value !== hCheck.as(Hex).value) {
+            this.state = Tlke.STATE_CONNECTION_FAILED;
+            return;
+        }
+        this.state = Tlke.STATE_CONNECTION_ESTABLISHED;
+        return {
+            inId: hCheck.bitSlice(16, 32),
+            outId: hCheck.bitSlice(0, 16),
+            key: hash(this.check.as(Bytes).concat(verified))
+        };
+    }
+
+    Algo.STATE_NOT_STARTED = 1;
+    Algo.STATE_AWAITING_OFFERDATA = 2;
+    Algo.STATE_AWAITING_OFFER_RESPONSE = 3;
+    Algo.STATE_AWAITING_AUTH = 4;
+    Algo.STATE_AWAITING_AUTHDATA = 5;
+    Algo.STATE_AWAITING_AUTH_RESPONSE = 6;
+    Algo.STATE_CONNECTION_ESTABLISHED = 7;
+    Algo.STATE_CONNECTION_SYNCED = 8;
+    Algo.STATE_CONNECTION_FAILED = -1;
+
+// __________________________________________________________________________ //
+
     // tl channel that is used during key exchange (while channel is being set up)
     function Tlke(factory) {
         invariant(factory, "Can be constructed only with factory");
@@ -40,22 +232,20 @@ define(function (require, exports, module) {
 
         // use the rng without serialization (assume singleton)
         this.random = factory.createRandom();
-        this.state = null;
-        this._dhAesKey = null;
-        this.dhk = null;
-        this.dh = null;
-        this.auth = null;
-        this.check = null;
-        this.authData = null;
+
+        this._algo = new Algo(this.random);
     }
 
     extend(Tlke.prototype, eventEmitter, serializable, {
+        getState: function () {
+            return this._algo.state;
+        },
         init: function () {
-            this.state = Tlke.STATE_NOT_STARTED;
+            this._algo.init();
         },
         generate: function () {
             this.checkEventHandlers();
-            invariant(this.state === Tlke.STATE_NOT_STARTED,
+            invariant(this.getState() === Algo.STATE_NOT_STARTED,
                 "Can't generate offer being in a state %s", this.state);
             this._generateOffer();
         },
@@ -75,18 +265,18 @@ define(function (require, exports, module) {
 
         processPacket: function (bytes) {
             invariant(bytes instanceof Multivalue, "bytes must be multivalue");
-            switch (this.state) {
-            case Tlke.STATE_AWAITING_OFFER_RESPONSE:
+            switch (this.getState()) {
+            case Algo.STATE_AWAITING_OFFER_RESPONSE:
                 this._acceptOfferResponse(bytes);
                 break;
-            case Tlke.STATE_AWAITING_AUTH_RESPONSE:
+            case Algo.STATE_AWAITING_AUTH_RESPONSE:
                 this._acceptAuthResponse(bytes);
                 break;
-            case Tlke.STATE_AWAITING_OFFERDATA:
+            case Algo.STATE_AWAITING_OFFERDATA:
                 this._acceptOfferData(bytes);
                 break;
-            case Tlke.STATE_AWAITING_AUTH:
-            case Tlke.STATE_AWAITING_AUTHDATA:
+            case Algo.STATE_AWAITING_AUTH:
+            case Algo.STATE_AWAITING_AUTHDATA:
                 this._acceptAuthData(bytes);
                 break;
             default:
@@ -96,181 +286,70 @@ define(function (require, exports, module) {
             this._onChanged();
         },
 
-        _encrypt: function (bytes, customKey) {
-            var iv = this._getRandomBytes(128);
-            var aes = new Aes(customKey || this._dhAesKey);
-            var encryptedData = aes.encryptCbc(bytes, iv);
-            return iv.as(Bytes).concat(encryptedData);
-        },
 
-        _decrypt: function (bytes, customKey) {
-            var dataBitArray = bytes.as(BitArray);
-            var iv = dataBitArray.bitSlice(0, 128);
-            var encryptedData = dataBitArray.bitSlice(128, dataBitArray.bitLength());
-            var aes = new Aes(customKey || this._dhAesKey);
-            return aes.decryptCbc(encryptedData, iv);
-        },
 
         // Alice 1.1 (instantiation)
         _generateOffer: function () {
-            this.dh = DiffieHellman.generate(Tlke.dhPrivBitLength, this.random);
-            var dhAes = this._getRandomBytes(Tlke.offerBitLength);
-            this._dhAesKey = dhAes;
-            var outId = dhAes.bitSlice(0, 16);
-            var inId = dhAes.bitSlice(16, 32);
+            this._algo._generateOffer();
             // emit this event before any "packet" event call to configure the appropriate transport behavior
             this.fire("addr", {inId: inId, outId: outId});
             this.fire("offer", this._dhAesKey);
-            this.state = Tlke.STATE_AWAITING_OFFER_RESPONSE;
-            this.fire("packet", this._getOfferData());
+            this._algo.state = Algo.STATE_AWAITING_OFFER_RESPONSE;
+            this.fire("packet", this._algo._getOfferData());
             this._onChanged();
         },
 
         // Bob 2.1 (instantiation) offer is from getOffer (via IM)
         _acceptOffer: function (offer) {
-            this.dh = DiffieHellman.generate(Tlke.dhPrivBitLength, this.random);
-            var dhAes = offer.as(Hex).as(BitArray);
-            this._dhAesKey = dhAes;
-            var inId = dhAes.bitSlice(0, 16);
-            var outId = dhAes.bitSlice(16, 32);
-            this.state = Tlke.STATE_AWAITING_OFFERDATA;
-            this.fire("addr", {inId: inId, outId: outId});
+            var ids = this._algo._acceptOffer(offer);
+            this.fire("addr", ids);
             this._onChanged();
         },
 
-        _getOfferData: function () {
-            var dhData = new Hex(this.dh.createKeyExchange());
-            return this._encrypt(dhData);
-        },
 
         // Bob 2.2.
         _acceptOfferData: function (bytes) {
-            var dhData;
-            try {
-                dhData = this._decrypt(bytes);
-            } catch (ex) {
-                console.warn("Received bad bytes.  " + ex.message);
-                return;
-            }
-            var dhDataHex = dhData.as(Hex).value;
-            var dhkHex = this.dh.decryptKeyExchange(dhDataHex);
-            this.dhk = new Hex(dhkHex);
-            this.state = Tlke.STATE_AWAITING_AUTH;
+            this._algo._acceptOfferData(bytes);
             this.fire("packet", this._getOfferResponse());
             this.fire("auth", null);
         },
 
-        _getOfferResponse: function () {
-            var dhData = new Hex(this.dh.createKeyExchange());
-            return this._encrypt(dhData);
-        },
-
         // Alice 3.1
         _acceptOfferResponse: function (data) {
-            var dhDataHex;
-            try {
-                dhDataHex = this._decrypt(data).as(Hex).value;
-            } catch (ex) {
-                console.warn("Received bad bytes.  " + ex.message);
-                return;
-            }
-            var dhkHex = this.dh.decryptKeyExchange(dhDataHex);
-            this.dhk = new Hex(dhkHex);
-
-            this.auth = this._getRandomBytes(Tlke.authBitLength);
-            this.check = this._getRandomBytes(128);
-            this.state = Tlke.STATE_AWAITING_AUTH_RESPONSE;
+            this._algo._acceptOfferResponse(data);
             this.fire("packet", this._getAuthData());
             this.fire("auth", this.auth);
         },
 
-        _getAuthData: function () {
-            return this._encrypt(this.check, this._getVerifiedDhk());
-        },
-
-        _getVerifiedDhk: function () {
-            var dhk = this.dhk.as(Bytes);
-            var auth = this.auth.as(Bytes);
-            return hash(dhk.concat(auth));
-        },
-
         // Bob 4.2
         _acceptAuthData: function (bytes) {
-            this.authData = bytes;
-            if (this.auth) {
-                this._acceptAuthAndData();
-            } else {
-                this.state = Tlke.STATE_AWAITING_AUTH;
-            }
+            this._algo._acceptAuthData();
         },
 
         // Bob 4.1
         _acceptAuth: function (auth) {
-            this.auth = auth;
-            if (this.authData) {
-                this._acceptAuthAndData();
-            } else {
-                this.state = Tlke.STATE_AWAITING_AUTHDATA;
-            }
+            this._algo._acceptAuth();
             this._onChanged();
         },
 
         // Bob 4.3 (4.1 + 4.2)
         _acceptAuthAndData: function () {
-            var bytes = this.authData;
-            // todo check's checksum and ACHTUNG if not match
-            var verified = this._getVerifiedDhk();
-            try {
-                this.check = this._decrypt(bytes, verified);
-            } catch (ex) {
-                console.warn("Received bad bytes.  " + ex.message);
-                return;
-            }
-            this.state = Tlke.STATE_CONNECTION_ESTABLISHED;
+            var keyReady = this._algo._acceptAuthAndData();
             this.fire("packet", this._getAuthResponse());
-            var hCheck = hash(this.check);
-            this.fire("keyReady", {
-                inId: hCheck.bitSlice(0, 16),
-                outId: hCheck.bitSlice(16, 32),
-                key: hash(this.check.as(Bytes).concat(verified))
-            });
+            this.fire("keyReady", idsKey);
             this._onChanged();
-        },
-
-        _getAuthResponse: function () {
-            var hCheck = hash(this.check);
-            return this._encrypt(hCheck, this._getVerifiedDhk());
         },
 
         // Alice 5
         _acceptAuthResponse: function (bytes) {
-            var verified = this._getVerifiedDhk(), hCheck;
-            try {
-                hCheck = this._decrypt(bytes, verified);
-            } catch (ex) {
-                console.warn("Received bad bytes.  " + ex.message);
-                return;
-            }
-            if (hash(this.check).as(Hex).value !== hCheck.as(Hex).value) {
-                this.state = Tlke.STATE_CONNECTION_FAILED;
-                return;
-            }
-            this.state = Tlke.STATE_CONNECTION_ESTABLISHED;
-            this.fire("keyReady", {
-                inId: hCheck.bitSlice(16, 32),
-                outId: hCheck.bitSlice(0, 16),
-                key: hash(this.check.as(Bytes).concat(verified))
-            });
+            var keyReady = this._algo._acceptAuthResponse();
+            this.fire("keyReady", keyReady);
         },
 
         _onChanged: function () {
             this.fire("changed", this);
         },
 
-        _getRandomBytes: function (bitLength) {
-            invariant(isFunction(this.random.bitArray), "random must implement IRandom");
-            return this.random.bitArray(bitLength);
-        },
 
         deserialize: function (packet, context) {
             var dto = packet.getData();
@@ -314,15 +393,15 @@ define(function (require, exports, module) {
     //    STATE_AWAITING_AUTH       |
     //    acceptAuth(auth)          |
 
-    Tlke.STATE_NOT_STARTED = 1;
-    Tlke.STATE_AWAITING_OFFERDATA = 2;
-    Tlke.STATE_AWAITING_OFFER_RESPONSE = 3;
-    Tlke.STATE_AWAITING_AUTH = 4;
-    Tlke.STATE_AWAITING_AUTHDATA = 5;
-    Tlke.STATE_AWAITING_AUTH_RESPONSE = 6;
-    Tlke.STATE_CONNECTION_ESTABLISHED = 7;
-    Tlke.STATE_CONNECTION_SYNCED = 8;
-    Tlke.STATE_CONNECTION_FAILED = -1;
+    Tlke.STATE_NOT_STARTED = Algo.STATE_NOT_STARTED;
+    Tlke.STATE_AWAITING_OFFERDATA = Algo.STATE_AWAITING_OFFERDATA;
+    Tlke.STATE_AWAITING_OFFER_RESPONSE = Algo.STATE_AWAITING_OFFER_RESPONSE;
+    Tlke.STATE_AWAITING_AUTH = Algo.STATE_AWAITING_AUTH;
+    Tlke.STATE_AWAITING_AUTHDATA = Algo.STATE_AWAITING_AUTHDATA;
+    Tlke.STATE_AWAITING_AUTH_RESPONSE = Algo.STATE_AWAITING_AUTH_RESPONSE;
+    Tlke.STATE_CONNECTION_ESTABLISHED = Algo.STATE_CONNECTION_ESTABLISHED;
+    Tlke.STATE_CONNECTION_SYNCED = Algo.STATE_CONNECTION_SYNCED;
+    Tlke.STATE_CONNECTION_FAILED = Algo.STATE_CONNECTION_FAILED;
 
     module.exports = Tlke;
 });
