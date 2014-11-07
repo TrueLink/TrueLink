1d7e69611ae1fae3d5d99b9cafa27a93b032f23a
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2014-11-07 08:33:52 +0200

    ...and cleaned tlht
    --HG--
    branch : splitting-into-files

--- Tlht/tlht-algo.js
+++ Tlht/tlht-algo.js
@@ -1,28 +1,17 @@
 "use strict";
 
-var tools = require("../modules/tools");
 var SHA1 = require("../modules/cryptography/sha1-crypto-js");
 var Hex = require("../modules/multivalue/hex");
 var BitArray = require("../modules/multivalue/bitArray");
-var Utf8String = require("../modules/multivalue/utf8string");
 var Bytes = require("../modules/multivalue/bytes");
 var Aes = require("../modules/cryptography/aes-sjcl");
 
-var eventEmitter = require("../modules/events/eventEmitter");
-var invariant = require("../modules/invariant");
 var Multivalue = require("../modules/multivalue/multivalue");
 var Tlec = require("./../Tlec/Tlec");
 
-var serializable = require("../modules/serialization/serializable");
-
-var extend = tools.extend;
-var isFunction = tools.isFunction;
 
 
 
-function hash(value) {
-    return SHA1(value).as(BitArray).bitSlice(0, 128);
-}
 
 // __________________________________________________________________________ //
 
@@ -32,7 +21,7 @@ function DecryptionFailedError(innerError) {
 
 // __________________________________________________________________________ //
 
-function Algo(random) {
+function TlhtAlgo(random) {
     this._random = random;
 
     this._dhAesKey = null;
@@ -40,52 +29,52 @@ function Algo(random) {
     this._hashEnd = null;
 }
 
-Algo.prototype.init = function (key) {
+TlhtAlgo.prototype.init = function (key) {
     invariant(this._random, "rng is not set");
     this._dhAesKey = key;
 }
 
-Algo.prototype.generate = function () {
+TlhtAlgo.prototype.generate = function () {
     this._hashStart = this._random.bitArray(128);
     var hashEnd = this._hashStart, i;
     for (i = 0; i < Tlec.HashCount; i += 1) {
-        hashEnd = hash(hashEnd);
+        hashEnd = this._hash(hashEnd);
     }
     return hashEnd;
 }
 
-Algo.prototype.isHashReady = function () {
+TlhtAlgo.prototype.isHashReady = function () {
     return !!this._hashStart && !!this._hashEnd;
 }
 
-Algo.prototype.getHashReady = function () {
+TlhtAlgo.prototype.getHashReady = function () {
     return { 
         hashStart: this._hashStart,
         hashEnd: this._hashEnd
     };
 }
 
-Algo.prototype.createMessage = function (raw) {
+TlhtAlgo.prototype.createMessage = function (raw) {
     var hx = new Bytes([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
     return this._encrypt(hx.concat(raw));
 }
 
-Algo.prototype.processMessage = function (bytes) {
+TlhtAlgo.prototype.processMessage = function (bytes) {
     var decryptedData = this._decrypt(bytes);
     return decryptedData.bitSlice(128, decryptedData.bitLength());
 }
 
-Algo.prototype.setHashEnd = function (hashEnd) {
+TlhtAlgo.prototype.setHashEnd = function (hashEnd) {
     this._hashEnd = hashEnd;
 }
 
-Algo.prototype.deserialize = function (data) {
+TlhtAlgo.prototype.deserialize = function (data) {
     this._dhAesKey = data.dhAesKey ? Hex.deserialize(data.dhAesKey) : null;
     this._hashStart = data.hashStart ? Hex.deserialize(data.hashStart) : null;
     this._hashEnd = data.hashEnd ? Hex.deserialize(data.hashEnd) : null;
 }
 
-Algo.prototype.serialize = function () {
+TlhtAlgo.prototype.serialize = function () {
     return {
         dhAesKey: this._dhAesKey ? this._dhAesKey.as(Hex).serialize() : null,
         hashStart: this._hashStart ? this._hashStart.as(Hex).serialize() : null,
@@ -93,7 +82,7 @@ Algo.prototype.serialize = function () {
     };
 }
 
-Algo.prototype._encrypt = function (bytes) {
+TlhtAlgo.prototype._encrypt = function (bytes) {
     invariant(this._dhAesKey, "channel is not configured");
     var iv = this._random.bitArray(128);
     var aes = new Aes(this._dhAesKey);
@@ -101,7 +90,7 @@ Algo.prototype._encrypt = function (bytes) {
     return iv.as(Bytes).concat(encryptedData);
 }
 
-Algo.prototype._decrypt = function (bytes) {
+TlhtAlgo.prototype._decrypt = function (bytes) {
     invariant(this._dhAesKey, "channel is not configured");
     var dataBitArray = bytes.as(BitArray);
     var iv = dataBitArray.bitSlice(0, 128);
@@ -115,109 +104,8 @@ Algo.prototype._decrypt = function (bytes) {
     }
 }
 
-// __________________________________________________________________________ //
-
-function Tlht(factory) {
-    invariant(factory, "Can be constructed only with factory");
-    invariant(isFunction(factory.createRandom), "factory must have createRandom() method");
-
-    this._factory = factory;
-    this._defineEvent("changed");
-    this._defineEvent("packet");
-    this._defineEvent("htReady");
-
-    this._readyCalled = false;
-    this._algo = new Algo(factory.createRandom());
+TlhtAlgo.prototype._hash = function (value) {
+    return SHA1(value).as(BitArray).bitSlice(0, 128);
 }
 
-extend(Tlht.prototype, eventEmitter, serializable, {
-    serialize: function (packet, context) {
-        var data = this._algo.serialize();
-        data.readyCalled = this._readyCalled;
-        packet.setData(data);
-    },
-    deserialize: function (packet, context) {
-        var data = packet.getData();
-        this._readyCalled = dto.readyCalled;
-        this._algo.deserialize(data);
-    },
-
-    init: function (key) {
-        invariant(key instanceof Multivalue, "key must be multivalue");
-        this._algo.init(key);
-        this.checkEventHandlers();
-        this._onChanged();
-    },
-
-    generate: function () {
-        console.log("Tlht generate");
-        var hashEnd = this._algo.generate();
-        var messageData = {
-            "t": "h",
-            "d": hashEnd.as(Hex).serialize()
-        };
-        this._onMessage(messageData);
-        if (this._algo.isHashReady()) {
-            console.log("hashes ready");
-            this._onHashReady();
-        }
-        this._onChanged();
-    },
-
-    _onMessage: function (messageData) {
-        var raw = new Utf8String(JSON.stringify(messageData));
-        var encrypted = this._algo.createMessage(raw);
-        this.fire("packet", encrypted);
-    },
-
-    // process packet from the network
-    processPacket: function (bytes) {
-        invariant(bytes instanceof Multivalue, "bytes must be multivalue");
-
-        var netData;
-        try {
-            netData = this._algo.processMessage(bytes);
-        } catch (ex) {
-            if (ex instanceof DecryptionFailedError) {
-                console.warn("Tlht failed to decrypt message. " + ex.innerError.message);
-                return; // not for me
-            } else {
-                throw ex;
-            }
-        }
-
-        var message;
-        try {
-            message = JSON.parse(netData.as(Utf8String).value);
-        } catch (ex) {
-            console.log("Tlht failed to parse message");
-            // not for me
-            return;
-        }
-
-        if (message.t === "h" && message.d) {
-            this._algo.setHashEnd(Hex.deserialize(message.d));
-            if (this._algo.isHashReady()) {
-                console.log("hashes ready");
-                this._onHashReady();
-            }
-        } else {
-            console.log("Tlht process packet, skiping some msg", message);
-        }
-    },
-
-    _onHashReady: function () {
-        if (this._readyCalled) { return; }
-        this._readyCalled = true;
-        this.fire("htReady", this._algo.getHashReady());
-        this._onChanged();
-    },
-
-    _onChanged: function () {
-        this.fire("changed", this);
-    }
-
-
-});
-
-module.exports = Tlht;
+module.exports = TlhtAlgo;
