a0f906cd4bfe198beb113df95a3b39fab31deb90
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2014-11-12 16:16:56 +0200

    removed modules/multivalue & modules/converters
    --HG--
    branch : tests

--- modules/converters/all.js
+++
@@ -1,9 +0,0 @@
-var a1 = require("./crypto-js");
-var a2 = require("./customTypes");
-var a3 = require("./sjcl");
-var a4 = require("./forge_wrapper");
-"use strict";
-//make typescript to actually load thos modules
-var _blank = { fake: [a1, a2, a3, a4] };
-var b = {};
-module.exports = b;
